03eaead415534c130f5454f5b35bdd01d676d970
Pavlo Sevidov <pavlosevidov@gmail.com>
2024-07-23 11:15:54 +0100

    Add publish to PyPI workflow

--- setup.py
+++ setup.py
@@ -3,11 +3,17 @@ from setuptools import setup, find_packages
 setup(
     name='zoe-utils',
     version='0.1.0',
+    author='Pavlo Sevidov',
+    author_email='pavlo.sevidov@datavise.ai',
+    description='A Zeo utility library for working with Prefect',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/pavlo-sevidov-datavise/zoe-utils',
     packages=find_packages(),
-    install_requires=[],
-    entry_points={
-        'console_scripts': [
-            'run-command=utilities.shell_utils:ShellTask'
-        ]
-    }
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.11',
 )
